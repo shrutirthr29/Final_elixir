0a9910137047f66b5a2598b66ba58439e9ff0129
Shruti Rathore <shray.shruti@gmail.com>
2023-01-21 23:52:19 +0530

    Update navdata.js (twitter id)

--- src/components/Navbar/navData.js
+++ src/components/Navbar/navData.js
@@ -20,7 +20,7 @@ export const navLinks = [
 export const socialLinks = [
   {
     label: 'Twitter',
-    url: 'https://twitter.com/CommunityElixir?t=HUeon9ryympVSDpoMvTSHg&s=09',
+    url: 'https://twitter.com/TheElixirTech',
     icon: 'twitter',
   },
   {
